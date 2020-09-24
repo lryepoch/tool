6503dc0dd080b43201489e4edf8d83e1218a6738
lryepoch <lryepoch@qq.com>
2020-09-24 22:59:18 +0800

    -create:   1.学习建造者模式；

--- designPattern/src/main/java/com/pattern/creational/singleton/SingletonTest.java
+++ designPattern/src/main/java/com/pattern/creational/singleton/SingletonTest.java
@@ -1,12 +1,6 @@
 package com.pattern.creational.singleton;
 
-/**
- * ����ģʽ
- *
- * @author lryepoch
- * @description ��֤һ�������һ��ʵ�������ṩһ����������ȫ�ַ��ʵ�
- * @date 2020/7/7 10:41
- */
+
 public class SingletonTest {
 
     public static void main(String[] args) {
