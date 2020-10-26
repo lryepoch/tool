9f0bddbef3c247e650ecb5824807055b1ad6c915
lryepoch <1783510791@qq.com>
2020-10-26 19:08:42 +0800

    -update:   1.新增；

--- example/src/main/java/thread/Wanger.java
+++ example/src/main/java/thread/Wanger.java
@@ -1,5 +1,7 @@
 package thread;
 
+import lombok.SneakyThrows;
+
 import java.util.concurrent.*;
 
 /**
@@ -37,19 +39,44 @@ public class Wanger {
         executorService.shutdown();*/
 
 
-        //3
-        ExecutorService executorService = new ThreadPoolExecutor(10, 1000, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
-        for (int i = 0; i < 1000; i++) {
-            Runnable r = new Runnable() {
+        /**
+        * @description 在for循环中使用线程池对象获取线程
+        * @author lryepoch
+        * @date 2020/10/17 11:32
+        *
+        */
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);
+        for (int i = 0; i < 10; i++) {
+            fixedThreadPool.execute(new Runnable() {
+                @SneakyThrows
                 @Override
                 public void run() {
-                    Wanger.addCount();
+                    System.out.println("what");
+                    Thread.sleep(2000);
                 }
-            };
-            executorService.execute(r);
+            });
         }
-        executorService.shutdown();
-        System.out.println(Wanger.count);
+
+        fixedThreadPool.shutdown();//关闭线程池
+        //此处不可以删除或注释，需要线程执行结束后再执行别的内容,即只有线程结束后才会继续向下执行
+        while (!fixedThreadPool.isTerminated()) {
+
+        }
+
+
+        //3
+//        ExecutorService executorService = new ThreadPoolExecutor(10, 1000, 60L, TimeUnit.SECONDS, new ArrayBlockingQueue<>(10));
+//        for (int i = 0; i < 1000; i++) {
+//            Runnable r = new Runnable() {
+//                @Override
+//                public void run() {
+//                    Wanger.addCount();
+//                }
+//            };
+//            executorService.execute(r);
+//        }
+//        executorService.shutdown();
+//        System.out.println(Wanger.count);
     }
 
     public static int count = 0;
