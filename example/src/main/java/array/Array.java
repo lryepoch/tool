1fa9003fdb88275b90498e4e560d1e5fe9cb48ed
lryepoch <lryepoch@qq.com>
2021-03-28 19:23:42 +0800

    2021-3-28

--- example/src/main/java/array/Array.java
+++ example/src/main/java/array/Array.java
@@ -8,12 +8,13 @@ import java.util.List;
  * @date 2019-12-17 2019/12/17
  * @description
  */
-public class Array {
-    public static void main(String[] args) {
-        Integer[] ids = new Integer[]{3,5,5,4};
-        List<Integer> list = Arrays.asList(ids);
-        System.out.println(list.getClass());
-        System.out.println(list);
-        System.out.println(Arrays.asList(ids));
-    }
-}
+//public class Array {
+//    public static void main(String[] args) {
+//        Integer[] ids = new Integer[]{3,5,5,4};
+//        List<Integer> list = Arrays.asList(ids);
+//        System.out.println(list.getClass());
+//        System.out.println(list);
+//        System.out.println(Arrays.asList(ids));
+//    }
+//}
+
